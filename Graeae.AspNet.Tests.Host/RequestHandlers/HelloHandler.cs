d9e67caa5223d51dfa0aa87fd7389c5773bf733b
Greg Dennis <gregsdennis@yahoo.com>
2023-10-17 09:55:56 +1300

    map from openapi desc

---
+++ Graeae.AspNet.Tests.Host/RequestHandlers/HelloHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace Graeae.AspNet.Tests.Host.RequestHandlers;
+
+[RequestHandler("/hello")]
+public static class HelloHandler
+{
+	public static Task<string> Get(HttpContext context)
+	{
+		return Task.FromResult("hello world");
+	}
+}
