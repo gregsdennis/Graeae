32a6d0af088da0ee49244672a70aef10a48e3482
Greg Dennis <gregsdennis@yahoo.com>
2023-10-20 09:03:10 +1300

    diagnostic created to warn about missing route handlers

---
+++ Graeae.AspNet.Analyzer/Diagnostics.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Graeae.AspNet.Analyzer;
+
+public static class Diagnostics
+{
+	public static Diagnostic NoPaths(string openApiFilePath) =>
+		Diagnostic.Create(new("GR0001", "No paths", $"No paths are defined in '{openApiFilePath}'", "Path generation", DiagnosticSeverity.Info, true), Location.None, DiagnosticSeverity.Info);
+
+	public static Diagnostic MissingRouteHandler(string route) =>
+		Diagnostic.Create(new("GR0001", "Route not handled", $"Found no handler type for route '{route}'", "Path generation", DiagnosticSeverity.Warning, true), Location.None, DiagnosticSeverity.Warning);
+}
